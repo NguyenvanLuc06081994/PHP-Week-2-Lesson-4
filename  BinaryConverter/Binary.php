0683df2cc70e1a2fb054a94f94988f28849d5150
NguyenvanLuc06081994 <nvluc0608@gmail.com>
2020-07-02 17:16:18 +0700

    update file

---  BinaryConverter/Binary.php
+++  BinaryConverter/Binary.php
@@ -1,3 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form action="" method="post">
+    <input type="text" name="number" placeholder="input number">
+    <input type="submit" value="Change">
+</form>
 <?php
 
 
@@ -10,18 +24,35 @@ class Binary
         $this->stack = array();
     }
 
-    public function push($number)
+    public function changeToBinary($number)
     {
         $arr = [];
         while ($number > 0) {
-            $result = (int)($number / 2);
-            $number1 = $number % 2;
-            $number = $result;
-            array_unshift($arr, $number1);
+            $result = $number % 2;
+            $number = (int)($number / 2);
+            array_unshift($arr, $result);
         }
-        return implode("", $arr);
+        return implode('', $arr);
     }
 
+    public function changeToNumber($number)
+    {
+        $newNumber = 0;
+        $arr = str_split($number);
+        $arrNew = array_reverse($arr);
+        for ($i = 0; $i < count($arrNew); $i++) {
+            $newNumber += $arrNew[$i] * 2 ** $i;
+        }
+        return $newNumber;
+    }
 }
-$binary = new Binary();
-echo($binary->push(10));
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $number = $_POST['number'];
+    $binary = new Binary();
+    echo $number . " chuyen sang so nhi phan la:";
+    echo $binary->changeToNumber($number);
+}
+?>
+</body>
+</html>
